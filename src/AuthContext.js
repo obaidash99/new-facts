e664a62abe3ee61d3ce10f7d69ad96db789bbed6
obaida <obaidashurbaji99@gmail.com>
2024-01-17 18:17:36 +0200

    add auth context

---
+++ src/AuthContext.js
@@ -0,0 +1,60 @@
+import {
+	createUserWithEmailAndPassword,
+	sendPasswordResetEmail,
+	signInWithEmailAndPassword,
+	signOut,
+	updateEmail,
+	updatePassword,
+} from 'firebase/auth';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { auth } from './firebase';
+
+const AuthContext = createContext();
+
+export function useAuth() {
+	return useContext(AuthContext);
+}
+
+export default function AuthProvider({ children }) {
+	const [loading, setloading] = useState(true);
+	const [currentUser, setCurrentUser] = useState();
+
+	function signin(email, password) {
+		return signInWithEmailAndPassword(auth, email, password);
+	}
+
+	function signup(email, password) {
+		return createUserWithEmailAndPassword(auth, email, password);
+	}
+
+	function logout() {
+		return signOut(auth);
+	}
+
+	function resetPassword(email) {
+		return sendPasswordResetEmail(auth, email);
+	}
+
+	function editEmail(email) {
+		return updateEmail(auth.currentUser, email);
+	}
+
+	function editPassword(password) {
+		return updatePassword(auth.currentUser, password);
+	}
+
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged((user) => {
+			setCurrentUser(user);
+			setloading(false);
+		});
+
+		return unsubscribe;
+	}, []);
+
+	const value = { currentUser, signin, signup, resetPassword, logout };
+
+	return (
+		<AuthContext.Provider value={value}>{!loading && children}</AuthContext.Provider>
+	);
+}
